33dafd69c4ce25de490e46a74263f4c796a65eda
lwingyikonan <140313445+lwingyikonan@users.noreply.github.com>
2023-07-23 16:39:26 +0630

    Create KaliGnome.sh

---
+++ KaliGnome.sh
@@ -0,0 +1,145 @@
+pkg up -y && pkg i -y x11-repo && pkg i -y proot-distro pulseaudio termux-x11-nightly 
+
+clear
+
+echo "    "
+
+{
+red='\033[1;31m'
+}
+echo -e "        
+$red
+          
+██╗  ██╗ █████╗ ██╗     ██╗    ██╗     ██╗███╗   ██╗██╗   ██╗██╗  ██╗    
+██║ ██╔╝██╔══██╗██║     ██║    ██║     ██║████╗  ██║██║   ██║╚██╗██╔╝    
+█████╔╝ ███████║██║     ██║    ██║     ██║██╔██╗ ██║██║   ██║ ╚███╔╝     
+██╔═██╗ ██╔══██║██║     ██║    ██║     ██║██║╚██╗██║██║   ██║ ██╔██╗     
+██║  ██╗██║  ██║███████╗██║    ███████╗██║██║ ╚████║╚██████╔╝██╔╝ ██╗    
+╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝    ╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝
+               KALI LINUX GNOME FOR Android
+
+"| pv -qL 600
+
+
+echo ""
+
+
+echo " "
+             
+             termux-setup-storage 
+             
+             echo "DISTRO_NAME='Kali (kali)'" >>~/../usr/etc/proot-distro/kali.sh
+             
+             echo ""
+             
+             proot-distro restore storage/shared/ADM/kali.tar.xz
+      
+      pid=$!
+       
+             #LWINGYI && KONAN#
+              
+              printf "\r I"
+              
+              sleep 0.3
+              
+              #LWINGYI && KONAN#
+              
+              printf "\r In"
+              
+              sleep 0.3
+              
+              #LWINGYI && KONAN#
+              
+              printf "\r Ins"
+              
+              sleep 0.3
+
+              #LWINGYI && KONAN#
+              
+              printf "\r Inst"
+              
+              sleep 0.3
+              
+              #LWINGYI && KONAN#
+              
+              printf "\r Insta"
+              
+              sleep 0.3
+              
+              #LWINGYI && KONAN#
+              
+              printf "\r Instal"
+              
+              sleep 0.3
+              
+              #LWINGYI && KONAN#
+              
+              printf "\r Install"
+              
+              sleep 0.3
+              
+              #LWINGYI && KONAN#
+              
+              printf "\r Install D"
+              
+              sleep 0.3
+              
+              #LWINGYI && KONAN#
+              
+              printf "\r Install Do"
+              
+              sleep 0.3
+              
+              #LWINGYI && KONAN#
+              
+              printf "\r Install Don"
+              
+              sleep 0.3
+              
+              #LWINGYI && KONAN#
+              
+              printf "\r Install Done"
+              
+              sleep 0.3
+             
+              
+              #LWINGYI && KONAN#
+              
+              printf "\r Install Done"
+              
+              sleep 0.3
+              
+                #LWINGYI && KONAN#
+              
+              printf "\r Install Done E"
+              
+              sleep 0.3
+              
+                #LWINGYI && KONAN#
+              
+              printf "\r Install Done Ex"
+              
+              sleep 0.3
+              
+              #LWINGYI && KONAN#
+              
+              printf "\r Install Done Exi"
+              
+              sleep 0.3
+              
+              #LWINGYI && KONAN#
+              
+              printf "\r Install Done Exit"
+              
+              sleep 0.3
+              
+              echo ""
+              
+              echo "proot-distro login kali --shared-tmp --bind /dev/null:/proc/sys/kernal/cap_last_cap" >>~/../usr/bin/kali
+              
+              
+              echo " "
+
+              chmod +x ~/../usr/bin/kali
+            
+            
